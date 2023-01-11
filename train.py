7e48634964e943e974d4d17d3020f8ee4c33815b
yehyunsuh <yehyun.suh@vanderbilt.edu>
2023-01-11 11:02:17 -0600

    [Feat] Heatmap #8

--- train.py
+++ train.py
@@ -1,6 +1,8 @@
 """
 reference:
-    https://github.com/aladdinpersson/Machine-Learning-Collection
+    heatmap: https://stackoverflow.com/questions/53467215/convert-pytorch-cuda-tensor-to-numpy-array
+             https://stackoverflow.com/questions/33282368/plotting-a-2d-heatmap 
+    train process: https://github.com/aladdinpersson/Machine-Learning-Collection
 """
 
 import os
@@ -26,7 +28,6 @@ def check_accuracy(loader, model, device):
 
     with torch.no_grad():
         for x, y in tqdm(loader):
-            # print("\n",tmp)
             tmp += 1
             x = x.to(device)
             y = y.to(device).unsqueeze(1)
@@ -68,28 +69,21 @@ def check_accuracy(loader, model, device):
     return label_accuracy, whole_image_accuracy, predict_as_label
 
 
-def save_predictions_as_imgs(loader, model, epoch, folder="plot_results", device="cuda"):
+def save_predictions_as_imgs(args, loader, model, epoch, folder="plot_results", device="cuda"):
     model.eval()
+    print("save")
 
-    if not os.path.exists(f'{folder}'):
-        os.mkdir(f'{folder}')
+    # if not os.path.exists(f'{folder}'):
+    #     os.mkdir(f'{folder}')
+    if not os.path.exists(f'{folder}/{args.wandb_name}'):
+        os.mkdir(f'{folder}/{args.wandb_name}')
 
     for idx, (x, y) in enumerate(loader):
         x = x.to(device=device)
         with torch.no_grad():
             preds = torch.sigmoid(model(x))
-            # print(preds[0][0][0])
-            printsave(preds[0][0][0])
+            # printsave(preds[0][0][0])
             # preds_binary = (preds > 0.5).float()
-            # print(preds_binary[0][0][0])
-        # exit()
-        Loss = "LogLoss"
-
-        ## reference: 
-        ## https://stackoverflow.com/questions/53467215/convert-pytorch-cuda-tensor-to-numpy-array
-        ## https://stackoverflow.com/questions/33282368/plotting-a-2d-heatmap 
-        # sns_pp = sns.heatmap(preds[0][0].detach().cpu().numpy())
-        # sns_pp.savefig(f'./plot_results/tmp_{epoch}.png', dpi=300)
 
         channel_0 = preds[0][0].detach().cpu().numpy()
         channel_1 = preds[0][1].detach().cpu().numpy()
@@ -98,35 +92,35 @@ def save_predictions_as_imgs(loader, model, epoch, folder="plot_results", device
         channel_4 = preds[0][4].detach().cpu().numpy()
         channel_5 = preds[0][5].detach().cpu().numpy()
 
-        if not os.path.exists('./plot_results/tmp0'):
-            os.mkdir(f'./plot_results/tmp0')
+        if not os.path.exists(f'./plot_results/{args.wandb_name}/label0'):
+            os.mkdir(f'./plot_results/{args.wandb_name}/label0')
         plt.imshow(channel_0, cmap='hot', interpolation='nearest')
-        plt.savefig(f'./plot_results/tmp0/tmp_{epoch}_0.png')
+        plt.savefig(f'./plot_results/{args.wandb_name}/label0/epoch_{epoch}.png')
 
-        if not os.path.exists('./plot_results/tmp1'):
-            os.mkdir(f'./plot_results/tmp1')
+        if not os.path.exists(f'./plot_results/{args.wandb_name}/label1'):
+            os.mkdir(f'./plot_results/{args.wandb_name}/label1')
         plt.imshow(channel_1, cmap='hot', interpolation='nearest')
-        plt.savefig(f'./plot_results/tmp1/tmp_{epoch}_1.png')
+        plt.savefig(f'./plot_results/{args.wandb_name}/label1/epoch_{epoch}.png')
 
-        if not os.path.exists('./plot_results/tmp2'):
-            os.mkdir(f'./plot_results/tmp2')
+        if not os.path.exists(f'./plot_results/{args.wandb_name}/label2'):
+            os.mkdir(f'./plot_results/{args.wandb_name}/label2')
         plt.imshow(channel_2, cmap='hot', interpolation='nearest')
-        plt.savefig(f'./plot_results/tmp2/tmp_{epoch}_2.png')
+        plt.savefig(f'./plot_results/{args.wandb_name}/label2/epoch_{epoch}.png')
 
-        if not os.path.exists('./plot_results/tmp3'):
-            os.mkdir(f'./plot_results/tmp3')
+        if not os.path.exists(f'./plot_results/{args.wandb_name}/label3'):
+            os.mkdir(f'./plot_results/{args.wandb_name}/label3')
         plt.imshow(channel_3, cmap='hot', interpolation='nearest')
-        plt.savefig(f'./plot_results/tmp3/tmp_{epoch}_3.png')
+        plt.savefig(f'./plot_results/{args.wandb_name}/label3/epoch_{epoch}.png')
 
-        if not os.path.exists('./plot_results/tmp4'):
-            os.mkdir(f'./plot_results/tmp4')
+        if not os.path.exists(f'./plot_results/{args.wandb_name}/label4'):
+            os.mkdir(f'./plot_results/{args.wandb_name}/label4')
         plt.imshow(channel_4, cmap='hot', interpolation='nearest')
-        plt.savefig(f'./plot_results/tmp4/tmp_{epoch}_4.png')
+        plt.savefig(f'./plot_results/{args.wandb_name}/label4/epoch_{epoch}.png')
 
-        if not os.path.exists('./plot_results/tmp5'):
-            os.mkdir(f'./plot_results/tmp5')
+        if not os.path.exists(f'./plot_results/{args.wandb_name}/label5'):
+            os.mkdir(f'./plot_results/{args.wandb_name}/label5')
         plt.imshow(channel_5, cmap='hot', interpolation='nearest')
-        plt.savefig(f'./plot_results/tmp5/tmp_{epoch}_5.png')
+        plt.savefig(f'./plot_results/{args.wandb_name}/label5/epoch_{epoch}.png')
 
         # torchvision.utils.save_image(preds_binary, f"{folder}/pred_{epoch}_{idx}.png")
         # torchvision.utils.save_image(preds_binary[0][0], f"{folder}/{Loss}_tmp_epoch{epoch}_0.png")
@@ -216,7 +210,7 @@ def train(args, DEVICE, model, loss_fn_pixel, loss_fn_geometry, optimizer, scale
         if best_loss > loss:
             print("=====New best model=====")
             torch.save(checkpoint, f"./results/best.pth")
-            save_predictions_as_imgs(val_loader, model, epoch, folder='./plot_results', device=DEVICE)
+            save_predictions_as_imgs(args, val_loader, model, epoch, folder='./plot_results', device=DEVICE)
             best_loss, count = loss, 0
         else:
             count += 1
