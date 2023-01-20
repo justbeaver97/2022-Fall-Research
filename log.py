4050a591dcc672fdd2c92c1540acaf12d8419f5a
yehyunsuh <yehyun.suh@vanderbilt.edu>
2023-01-20 10:21:42 -0600

    [Feat] Log Label Accuracy #16

--- log.py
+++ log.py
@@ -1,18 +1,40 @@
 import wandb
 
 def initiate_wandb(args):
-    wandb.init(
-        project=f"{args.wandb_project}", 
-        entity=f"{args.wandb_entity}",
-        name=f"{args.wandb_name}"
-    )
+    if args.wandb:
+        wandb.init(
+            project=f"{args.wandb_project}", 
+            entity=f"{args.wandb_entity}",
+            name=f"{args.wandb_name}"
+        )
 
-def log_results(
-    args, train_loss, label_accuracy, segmentation_accuracy, predict_as_label
-    ):
+# def log_results(
+#     args, train_loss, label_accuracy, segmentation_accuracy, predict_as_label
+#     ):
+#     wandb.log({
+#         'train loss': train_loss,
+#         'whole image accuracy': segmentation_accuracy,
+#         # 'label accuracy': label_accuracy,
+#         # 'predicted as label': predict_as_label,
+#     })
+
+def log_results_no_label(args, train_loss, loss_pixel, loss_geometry, segmentation_accuracy, dice_score):
     wandb.log({
         'train loss': train_loss,
-        'label accuracy': label_accuracy,
+        'pixel loss': loss_pixel,
+        'geometry loss': loss_geometry,
         'whole image accuracy': segmentation_accuracy,
+        'dice score': dice_score
+    })
+
+def log_results(args, train_loss, loss_pixel, loss_geometry, label_accuracy, label_accuracy2, segmentation_accuracy, predict_as_label, dice_score):
+    wandb.log({
+        'train loss': train_loss,
+        'pixel loss': loss_pixel,
+        'geometry loss': loss_geometry,
+        'whole image accuracy': segmentation_accuracy,
+        'label accuracy(1) - Preds/GT': label_accuracy,
+        'label accuracy(2) - GT/Preds': label_accuracy2,
         'predicted as label': predict_as_label,
+        'dice score': dice_score
     })
