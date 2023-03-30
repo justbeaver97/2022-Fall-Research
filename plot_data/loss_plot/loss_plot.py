4bc804ac9bb23193a83ba870bdf46e22443f4adc
yehyunsuh <yehyun.suh@vanderbilt.edu>
2023-03-30 12:32:08 -0500

    [Feat] Loss Graph & Prediction Plot #23

--- plot_data/loss_plot/loss_plot.py
+++ plot_data/loss_plot/loss_plot.py
@@ -7,6 +7,10 @@ Reference:
         https://stackoverflow.com/questions/17543359/drawing-lines-between-two-plots-in-matplotlib
     Sub plot set label:
         https://stackoverflow.com/questions/6963035/how-to-set-common-axes-labels-for-subplots
+    twinx to plot 2 graphs in 1 plot:
+        https://rfriend.tistory.com/689
+    subplot tick fontsize:
+        https://stackoverflow.com/questions/38369188/set-size-of-ticks-in-all-subplots
 """
 
 import argparse
@@ -15,35 +19,6 @@ import matplotlib.pyplot as plt
 from matplotlib.patches import ConnectionPatch
 
 from PIL import Image
-
-
-def convert_txt_to_list(txt_file):
-    rmse_list = [[0]*18 for _ in range(6)]
-
-    with open(f"./txt_files/{txt_file}", "r") as f:
-        for line in f:
-            data1 = list(line.split(','))
-    
-    for i in range(len(data1)):
-        rmse_value = data1[i].translate({ord('['): None})
-        rmse_value = rmse_value.translate({ord(']'): None})
-        rmse_list[i//18][i%18] = float(rmse_value)
-
-    return rmse_list
-
-
-def convert_list_to_dataframe(total_list, total_name):
-    data = []
-    for k in range(len(total_list)):
-        for i in range(len(total_list[k])):
-            for j in range(len(total_list[k][i])):
-                data.append([i, total_list[k][i][j], total_name[k]])
-
-    return pd.DataFrame(data, columns=["label", "value", "experiment"])
-
-
-def box_plot(df, name):
-    pass
 
 
 def main(args):
@@ -56,50 +31,57 @@ def main(args):
     rows = 2; cols = 3
 
     ax1 = fig.add_subplot(rows, cols, 1)
-    ax1.set_title('Epoch 0')
+    ax1.set_title('Epoch 0', fontsize=14)
     ax1.grid(False)
     ax1.set_xticks([])  ## To remove x grid
     ax1.set_yticks([])  ## To remove y grid
     ax1.imshow(image1)
 
     ax2 = fig.add_subplot(rows, cols, 2)
-    ax2.set_title('Epoch 99')
+    ax2.set_title('Epoch 99', fontsize=14)
     ax2.grid(False)
     ax2.set_xticks([])
     ax2.set_yticks([])
     ax2.imshow(image2)
 
     ax3 = fig.add_subplot(rows, cols, 3)
-    ax3.set_title('Epoch 249')
+    ax3.set_title('Epoch 249', fontsize=14)
     ax3.grid(False)
     ax3.set_xticks([])
     ax3.set_yticks([])
     ax3.imshow(image3)
     
     ##TODO: Convert .csv to line graph
-    df = pd.read_csv(f'./csv_files/{args.experiment}.csv')
+    df_train_loss = pd.read_csv(f'./csv_files/{args.experiment}_train_loss.csv')
     ax4 = fig.add_subplot(212)
-    ax4.set_xlabel('Epoch')
-    ax4.set_ylabel('Train Loss')
-    ax4.plot(df['Step'], df['Train Loss'])
+    ax4.set_xlabel('Epoch', fontsize=13)
+    ax4.set_ylabel('Train Loss', fontsize=13)
+    ax4.tick_params(axis='both', which='major', labelsize=13)
+    ax4.plot(df_train_loss['Step'], df_train_loss['Train Loss'])
+
+    df_rmse = pd.read_csv(f'./csv_files/{args.experiment}_rmse.csv')
+    ax5 = ax4.twinx() 
+    ax5.set_ylabel('RMSE', fontsize=13)
+    ax5.tick_params(axis='both', which='major', labelsize=13)
+    ax5.plot(df_rmse['Step'], df_rmse['Average RMSE'], color="black")
     
-    
-    plt.savefig('tmp.png') 
+    ax4.legend(['Train Loss'], loc='best', fontsize=13)
+    ax5.legend(['RMSE'], loc='upper center', fontsize=13)
 
     ##TODO: Draw vertical line
     fig.tight_layout()
 
-    xy1 = (1, 0.99)
+    xy1 = (1, 1.24)
     xy2 = (255, 511)
     con = ConnectionPatch(xyA=xy1, xyB=xy2, coordsA="data", coordsB="data", axesA=ax4, axesB=ax1, color="red")
     ax4.add_artist(con)
 
-    xy1 = (99, 0.99)
+    xy1 = (99, 1.05)
     xy2 = (255, 511)
     con = ConnectionPatch(xyA=xy1, xyB=xy2, coordsA="data", coordsB="data", axesA=ax4, axesB=ax2, color="red")
     ax4.add_artist(con)
 
-    xy1 = (249, 0.99)
+    xy1 = (249, 0.98)
     xy2 = (255, 511)
     con = ConnectionPatch(xyA=xy1, xyB=xy2, coordsA="data", coordsB="data", axesA=ax4, axesB=ax3, color="red")
     ax4.add_artist(con)
@@ -112,7 +94,7 @@ if __name__ == '__main__':
 
     parser.add_argument('--image1', type=str, default="epoch0_val0_pred_gt.png")
     parser.add_argument('--image2', type=str, default="epoch99_val0_pred_gt.png")
-    parser.add_argument('--image3', type=str, default="epoch299_val0_pred_gt.png")
+    parser.add_argument('--image3', type=str, default="epoch249_val0_pred_gt.png")
 
     parser.add_argument('--experiment', type=str, default="MIDL_D60-5_W_P_progressive_weighted_erosion_ver2_1e-4_50")
     parser.add_argument('--plot_name', type=str, default="2023_MIDL_Loss_Graph")
